d483ff4697b7fd79487319396a6c1505ecc6d654
Josh Castro <castrojoshua805@gmail.com>
2020-10-09 19:53:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,6 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each_with_index do |names, index|
-   puts `Happy Birthday #{names}! You are now #{age + 1} years old!`
+   p `Happy Birthday #{names}! You are now #{age + 1} years old!`
  end 
 end 
